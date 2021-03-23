eab4135a51133a7afb2638b3249f5f4d200dcef2
tmdeh <yus174113@gmail.com>
2021-03-23 15:15:07 +0900

    급식

--- NeisMeal/Form1.Designer.cs
+++ NeisMeal/Form1.Designer.cs
@@ -29,21 +29,57 @@ namespace NeisMeal
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 329);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(266, 59);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 329);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(535, 59);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(226, 329);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- NeisMeal/Form1.cs
+++ NeisMeal/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace NeisMeal
 {
@@ -26,8 +27,63 @@ namespace NeisMeal
             webClient.Headers["Contents-Type"] = "application/json";
             webClient.Encoding = Encoding.UTF8;
 
-            Stream stream = webClient.OpenRead("https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240454&MMEAL_SC_CODE=3&MLSV_YMD=20210323");
-            // json 값을 분석
+            Stream stream = webClient.OpenRead("https://open.neis.go.kr/hub/mealServiceDietInfo?Type=json&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240454&MLSV_YMD=20210323");
+
+            Root root = new Root();
+            StreamReader reader = new StreamReader(stream);
+            root = JsonConvert.DeserializeObject<Root>(reader.ReadToEnd());
+            textBox1.Text = root.mealServiceDietInfo[1].row[0].DDISH_NM.Replace("<br/>", "\r\n");
+            textBox2.Text = root.mealServiceDietInfo[1].row[1].DDISH_NM.Replace("<br/>", "\r\n");
+            textBox3.Text = root.mealServiceDietInfo[1].row[2].DDISH_NM.Replace("<br/>", "\r\n");
+            //textBox1.Text = reader.ReadToEnd();
+
+
+
         }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class RESULT
+    {
+        public string CODE { get; set; }
+        public string MESSAGE { get; set; }
+    }
+
+    public class Head
+    {
+        public int list_total_count { get; set; }
+        public RESULT RESULT { get; set; }
+    }
+
+    public class Row
+    {
+        public string ATPT_OFCDC_SC_CODE { get; set; }
+        public string ATPT_OFCDC_SC_NM { get; set; }
+        public string SD_SCHUL_CODE { get; set; }
+        public string SCHUL_NM { get; set; }
+        public string MMEAL_SC_CODE { get; set; }
+        public string MMEAL_SC_NM { get; set; }
+        public string MLSV_YMD { get; set; }
+        public string MLSV_FGR { get; set; }
+        public string DDISH_NM { get; set; }
+        public string ORPLC_INFO { get; set; }
+        public string CAL_INFO { get; set; }
+        public string NTR_INFO { get; set; }
+        public string MLSV_FROM_YMD { get; set; }
+        public string MLSV_TO_YMD { get; set; }
+    }
+
+    public class MealServiceDietInfo
+    {
+        public List<Head> head { get; set; }
+        public List<Row> row { get; set; }
+    }
+
+    public class Root
+    {
+        public List<MealServiceDietInfo> mealServiceDietInfo { get; set; }
     }
 }
